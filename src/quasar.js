a39e44160720b0823d6b59f3f6cfa7bc59117460
encounter1105 <1459722320@qq.com>
2021-05-11 17:25:48 +0800

    头像上传组件

--- src/quasar.js
+++ src/quasar.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 
 import "./styles/quasar.scss";
 import "@quasar/extras/material-icons/material-icons.css";
-import { Quasar,Platform,Notify  } from "quasar";
+import { Quasar,Platform,Notify,Dialog  } from "quasar";
+import "@quasar/extras/material-icons/material-icons.css";
+import "@quasar/extras/material-icons-outlined/material-icons-outlined.css";
 
-Vue.use(Quasar,Platform, {
+Vue.use(Quasar, {
   config: {},
-  plugins: {Notify,Platform},
+  plugins: {Notify,Platform,Dialog},
   extras: [
     'ionicons-v4',
     'eva-icons',
